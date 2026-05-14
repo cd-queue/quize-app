92f087bd81f50e05b228b5fd29e725908b01d97e
Aja <au4588219@gmail.com>
2026-05-14 21:58:22 +0530

    add correct incorrect answer feature

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const questions = [
         question: "Which header file is needed for printf()?",
         answers: [
             { text: 'stdio.h', correct: true },
-            { text: 'math.h', correct: true },
+            { text: 'math.h', correct: false },
             { text: 'conio.h', correct: false },
             { text: 'string.h', correct: false },
     
@@ -100,3 +100,58 @@ const questions = [
         ]
     }
 ]
+
+const questionElement = document.getElementById("question");
+const  answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + '. ' + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add('btn');
+        answerButtons.appendChild(button);
+        if(answer.correct)
+        {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+
+}
+
+function selectAnswer(e){
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectBtn.classList.add("correct");
+    }
+    else {
+        selectBtn.classList.add("incorrect");
+    }
+}
+
+startQuiz();
+
