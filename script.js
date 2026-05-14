7857997bb5c80ee75516ee6705e19e1442e978aa
Aja <au4588219@gmail.com>
2026-05-14 18:08:45 +0530

    add style and data in js

---
+++ script.js
@@ -0,0 +1,102 @@
+const questions = [
+    {
+        question: "Which language is known as the mother of programming languages?",
+        answers: [
+            { text: 'Java', correct: false },
+            { text: 'Python', correct: false },
+            { text: 'C', correct: true },
+            { text: 'HTML', correct: false },
+    
+        ]
+    },
+    {
+        question: "Which symbol is used to end a statement in C/C++?",
+        answers: [
+            { text: ':', correct: false },
+            { text: ';', correct: true },
+            { text: '.', correct: false },
+            { text: ',', correct: false },
+    
+        ]
+    },
+    {
+        question: "Which keyword is used to create a function in Python?",
+        answers: [
+            { text: 'func', correct: false },
+            { text: 'define', correct: false },
+            { text: 'function', correct: false },
+            { text: 'def', correct: true },
+    
+        ]
+    },
+    {
+        question: "Which loop is best when number of iterations is known?",
+        answers: [
+            { text: 'while', correct: false },
+            { text: 'do while', correct: false },
+            { text: 'for', correct: true },
+            { text: 'foreach', correct: false },
+    
+        ]
+    },
+    {
+        question: "Which operator means “not equal”?",
+        answers: [
+            { text: '!=', correct: true },
+            { text: '=', correct: false },
+            { text: '==', correct: false },
+            { text: '<=', correct: false },
+    
+        ]
+    },
+    {
+        question: "What is the size of int in most C compilers?",
+        answers: [
+            { text: '1 byte', correct: false },
+            { text: '2 byte', correct: false },
+            { text: '4 byte', correct: true },
+            { text: '8 byte', correct: false },
+    
+        ]
+    },
+    {
+        question: "Which data structure uses LIFO?",
+        answers: [
+            { text: 'Queue', correct: false },
+            { text: 'Stack', correct: true },
+            { text: 'Array', correct: false },
+            { text: 'Tree', correct: false },
+    
+        ]
+    },
+    {
+        question: "Which function is used for output in C?",
+        answers: [
+            { text: 'cin', correct: false },
+            { text: 'print()', correct: false },
+            { text: 'printf()', correct: true },
+            { text: 'cout', correct: false },
+    
+        ]
+    },
+    {
+        question: "Which header file is needed for printf()?",
+        answers: [
+            { text: 'stdio.h', correct: true },
+            { text: 'math.h', correct: true },
+            { text: 'conio.h', correct: false },
+            { text: 'string.h', correct: false },
+    
+        ]
+    },
+    {
+        question: "Which keyword is used for inheritance in C++",
+        answers: [
+            { text: 'extends', correct: false },
+            { text: 'inherit', correct: false },
+            { text: ':', correct: true },
+            { text: 'super', correct: false },
+    
+        ]
+    }
+]
